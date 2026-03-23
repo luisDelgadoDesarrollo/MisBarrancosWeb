77459f9a39772d71a45eaede821a36eae2d5fc2d
Luis Delgado <delgadofernandez.luisdesarrollo@gmail.com>
2026-03-23 17:09:18 +0100

    fix ads 3 try

--- src/dumbComponents/modal/AdModal.vue
+++ src/dumbComponents/modal/AdModal.vue
@@ -2,33 +2,37 @@
   <v-dialog v-model="showAd">
     <v-card style="max-height: 90vh">
       <v-col>
-        <v-img v-if="props.ad?.image"
+        <v-img
+          v-if="props.ad?.image"
           :src="imageUrl"
           alt="No se puede ver la imagen"
           class="ad-image"
-          contain @click="goToLink"
+          contain
+          @click="goToLink"
         />
         <p v-if="props.ad?.info">{{ props.ad?.info }}</p>
-        <p v-if="props.ad?.code"><strong>{{ props.ad?.code }}</strong></p>
-        <a v-if="props.ad?.link" :href="props.ad?.link" target="_blank" @click="onLinkClick">Enlace</a>
+        <p v-if="props.ad?.code">
+          <strong>{{ props.ad?.code }}</strong>
+        </p>
+        <a v-if="props.ad?.link" :href="props.ad?.link" target="_blank" @click="onLinkClick"
+          >Enlace</a
+        >
         <v-row justify="end">
           <v-btn @click="close">Cerrar</v-btn>
         </v-row>
       </v-col>
     </v-card>
-
   </v-dialog>
 </template>
 <script setup lang="ts">
-import { type Ad } from '@/api';
-import { adApi, imageApi } from '@/api/apiConfig';
-import { onMounted, ref, watch } from 'vue';
+import { type Ad } from '@/api'
+import { adApi, imageApi } from '@/api/apiConfig'
+import { onMounted, ref, watch } from 'vue'
 
 const imageUrl = ref<string | undefined>(undefined)
 
-
-const onLinkClick = () =>{
-  adApi.createAdLog({body: props.ad!.adId!})
+const onLinkClick = () => {
+  adApi.createAdLog({ body: props.ad!.adId! })
 }
 const props = withDefaults(
   defineProps<{
@@ -46,30 +50,13 @@ const goToLink = () => {
 const close = () => {
   showAd.value = false
 }
-onMounted(async () => {
-  if (props.ad) {
-    if (props.ad.image) {
-      try {
-        const response = await imageApi.imagesImageDirGet({
-          image: props.ad.image,
-          dir: 'ad',
-        })
-        imageUrl.value = URL.createObjectURL(response)
-      } catch (e) {
-        console.error('Error loading image', e)
-        imageUrl.value = undefined
-      }
-    }
-
-    showAd.value = true
-  }
-})
 watch(
   () => props.ad,
   async (newAd) => {
-    if (!newAd) return
-
-    // cargar imagen si existe
+    if (!newAd || !newAd.adId) {
+      showAd.value = false
+      return
+    }
     if (newAd.image) {
       try {
         const response = await imageApi.imagesImageDirGet({
@@ -83,16 +70,13 @@ watch(
       }
     }
 
-    // abrir diálogo
     showAd.value = true
   },
-  { immediate: true } // 👈 ejecuta la primera vez también
+  { immediate: true },
 )
-
-
 </script>
 <style scoped>
-  .ad-image {
+.ad-image {
   max-width: 100%;
   max-height: 70vh; /* Ajusta el % de la pantalla que quieres usar */
   object-fit: contain;
